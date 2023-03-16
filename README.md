02fbafd72d35c8e5dab0b2c6c146b201d967786f
mv-31 <96203101+mv-31@users.noreply.github.com>
2023-03-16 21:09:48 +0300

    V0.1 16.03.2023

---
+++ README.md
@@ -0,0 +1,44 @@
+# QA GURU Python 4 поток
+### Задание 7
+
+
+
+## Как запустить проект на Windows:
+
+Клонировать репозиторий и перейти в него в командной строке:
+
+```
+git clone https://github.com/wombatoff/qa_guru_python_4_7
+```
+
+```
+cd qa_guru_python_4_7
+```
+
+Создать и активировать виртуальное окружение:
+
+```
+python -m venv venv
+source venv/Scripts/activate
+```
+
+
+Установить зависимости из файла requirements.txt:
+
+```
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+```
+
+## Запуск теста
+
+```
+pytest
+```
+
+
+---
+
+## Технологии проекта
+
+- Python
---
+++ test_qa_guru_python_4_7.py
@@ -0,0 +1,50 @@
+import hashlib
+import io
+import os
+import zipfile
+
+import requests
+
+
+def download_file(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return io.BytesIO(response.content)
+
+
+def get_filename_from_url(url):
+    return url.split("/")[-1]
+
+
+def calculate_checksum(file):
+    file.seek(0)
+    file_hash = hashlib.md5()
+    for chunk in iter(lambda: file.read(4096), b""):
+        file_hash.update(chunk)
+    return file_hash.hexdigest()
+
+
+urls = [
+    "https://github.com/qa-guru/qa_guru_python_4_7/raw/master/file_example_XLSX_10.xlsx",
+    "https://github.com/qa-guru/qa_guru_python_4_7/raw/master/docs-pytest-org-en-latest.pdf",
+    "https://github.com/qa-guru/qa_guru_python_4_7/raw/master/username.csv",
+]
+
+files = [(download_file(url), get_filename_from_url(url)) for url in urls]
+
+
+def test_create_archive():
+    with zipfile.ZipFile("resources/archive.zip", mode="w") as archive:
+        for file, filename in files:
+            archive.writestr(filename, file.getvalue())
+
+    assert os.path.exists("resources/archive.zip"), "The archive was not created"
+
+
+def test_files_checksum():
+    with zipfile.ZipFile("resources/archive.zip", mode="r") as archive:
+        for file, filename in files:
+            file_checksum = calculate_checksum(file)
+            with archive.open(filename, "r") as archived_file:
+                archived_file_checksum = calculate_checksum(archived_file)
+                assert file_checksum == archived_file_checksum, f"Checksum mismatch for {filename}"
